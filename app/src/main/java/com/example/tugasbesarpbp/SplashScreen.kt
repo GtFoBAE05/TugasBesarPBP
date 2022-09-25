babc13d01c7ccad563284a0292699b3981eebdbd
GtFoBAE05 <y.imannuel@gmail.com>
2022-09-25 14:54:07 +0700

    fix splash screen hanya muncul pertama install

--- app/src/main/java/com/example/tugasbesarpbp/SplashScreen.kt
+++ app/src/main/java/com/example/tugasbesarpbp/SplashScreen.kt
@@ -1,6 +1,8 @@
 package com.example.tugasbesarpbp
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
@@ -8,6 +10,9 @@ import androidx.appcompat.app.AppCompatActivity
 
 @Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
@@ -19,12 +24,35 @@ class SplashScreen : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        // we used the postDelayed(Runnable, time) method
-        // to send a message with a delayed time.
-        Handler().postDelayed({
+        var sharedPreferences= getSharedPreferences("preferences", MODE_PRIVATE)
+
+        val firstTimeRun= sharedPreferences.getString("firstTimeRun","")
+
+        if(firstTimeRun.equals("false")){
+            println("masuk pertama")
+
+            Handler().removeCallbacks({ this })
             val intent = Intent(this, LoginPage::class.java)
             startActivity(intent)
-            finish()
-        }, 3000) // 3000 is the delayed time in milliseconds.
+
+
+        }else{
+            println("masuk kedua")
+            val editor: SharedPreferences.Editor= sharedPreferences!!.edit()
+            editor.putString("firstTimeRun","false")
+            editor.apply()
+
+            Handler().postDelayed({
+                val intent = Intent(this, LoginPage::class.java)
+                startActivity(intent)
+                finish()
+            }, 3000) // 3000 is the delayed time in milliseconds.
+
+
+        }
+
+
+
+
     }
 }
