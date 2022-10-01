d5d21b6ef72ec46bb7564fcfe49cd3a4c6e978a8
DominikusWibi <wibidanang456@gmail.com>
2022-10-01 08:49:05 +0700

    tambahan big picture

--- app/src/main/java/notif/NotificationReceiver.kt
+++ app/src/main/java/notif/NotificationReceiver.kt
@@ -3,11 +3,17 @@ package com.example.tugasbesarpbp
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 
 class NotificationReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         val message = intent!!.getStringExtra("toastMessage")
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+
+        val builder .setStyle(
+        NotificationCompat.BigPictureStyle()
+            .bigPicture(BitmapFactory.decodeResource(getResources(), R.drawable.uang_depo)))
     }
 }
